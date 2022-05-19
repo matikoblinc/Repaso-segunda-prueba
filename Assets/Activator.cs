ccb1657ea769b4fcfb34be5649f053f67555c0d8
matikoblinc <78440014+matikoblinc@users.noreply.github.com>
2022-05-19 15:57:08 -0300

    Exercise finished

---
+++ Assets/Activator.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Activator : MonoBehaviour
+{
+    public GameObject myCube;
+    bool Presionado;
+
+
+    void Start()
+    {
+        myCube.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if (Input.GetKeyDown(KeyCode.Space))
+        //{
+        //    if (Presionado == false)
+        //    {
+        //        myCube.SetActive(true);
+        //        Presionado = true;
+        //    }
+        //    else
+        //    {
+        //        myCube.SetActive(false);
+        //        Presionado = false;
+        //    }
+
+        // el if de arriba y el de abajo hacen lo mismo 
+
+        //if (Input.GetKeyDown(KeyCode.Space))
+        //{
+        //    if (myCube.activeInHierarchy == false) 
+        //    {
+        //        myCube.SetActive(true);
+        //        Presionado = true;
+        //    }
+        //    else
+        //    {
+        //        myCube.SetActive(false);
+        //        Presionado = false;
+        //    }
+
+        //}
+
+        // forma mas corta de hacerlo:
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            myCube.SetActive(!myCube.activeInHierarchy);
+        }
+    }
+}
---
+++ Assets/instantiator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class instantiator : MonoBehaviour
+{
+
+    public GameObject prefab;
+    public int num1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            for (int i = 0; i < num1; i++) {
+                GameObject clon;
+                clon = Instantiate(prefab);
+
+                Destroy(clon, 0.5f); // que sestruya el clon luego de 1 segundo. 
+            }
+        }
+    }
+}
